565e84994b870fc59cebf21c9cdc93c191220ea8
Jake Broughton <jake.broughton@seqera.io>
2025-05-22 02:57:31 +0200

    Add booking buttons to HP

--- src/components/AvailablePeriods/index.tsx
+++ src/components/AvailablePeriods/index.tsx
@@ -7,19 +7,24 @@ import Item from './Item';
 type Props = {
   rates: RatesResponse;
   availability: Availability;
-  currencyCode: string;
+  currencyCode?: string | null;
   propertyId: number;
+  limit?: number;
 };
 
-const AvailablePeriods = ({ rates, availability, currencyCode, propertyId }: Props) => {
-  const [desiredStay, setDesiredStay] = useState(0);
-  const periods = findAvailablePeriods(rates, availability, desiredStay)?.slice(0, 4);
-  console.log(rates);
+const AvailablePeriods = ({
+  rates,
+  availability,
+  currencyCode = 'EUR',
+  propertyId,
+  limit = 4,
+}: Props) => {
+  const [desiredStay, setDesiredStay] = useState(7);
+  const periods = findAvailablePeriods(rates, availability, desiredStay)?.slice(0, limit);
   return (
     <div className="space-y-4">
-      <h3 className="text-xl font-semibold">Next Available Periods</h3>
       <div>
-        <select onChange={(e) => setDesiredStay(Number(e.target.value))} value={7}>
+        <select onChange={(e) => setDesiredStay(Number(e.target.value))}>
           <option value={2}>2 nights</option>
           <option value={3}>3 nights</option>
           <option value={4}>4 nights</option>
@@ -30,7 +35,12 @@ const AvailablePeriods = ({ rates, availability, currencyCode, propertyId }: Pro
       </div>
       <div className="grid gap-4">
         {periods.map((period, index) => (
-          <Item key={index} period={period} currencyCode={currencyCode} propertyId={propertyId} />
+          <Item
+            key={propertyId + index}
+            period={period}
+            currencyCode={currencyCode || 'EUR'}
+            propertyId={propertyId}
+          />
         ))}
       </div>
     </div>
