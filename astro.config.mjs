85a719c2bd55713773425605ef3e5cb6a0c67e58
Jake Broughton <jake.broughton@seqera.io>
2025-05-23 23:12:51 +0200

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,10 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  image: {
+    experimentalLayout: 'constrained',
+  },
+  experimental: {
+    responsiveImages: true,
+  },
 });
