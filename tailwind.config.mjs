79ff28941ddfe099871acf3aea707316d4095485
Jake Broughton <jake.broughton@seqera.io>
2025-05-24 22:11:28 +0200

    a

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,23 @@ import { screens } from './tailwind-constants.mjs';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
-    screens
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat Variable', 'Montserrat', 'sans-serif'],
+      },
+    },
+    screens,
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1600px',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
